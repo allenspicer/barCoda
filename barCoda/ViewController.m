39f7dc7a79b57ba1077e6a5665c2e6f5bf4555ee
Allen Spicer <allenspicer@gmail.com>
2016-06-08 11:09:12 -0400

    Initial Commit

---
+++ barCoda/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  barCoda
+//
+//  Created by Allen Spicer on 6/8/16.
+//  Copyright © 2016 Allen Spicer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) UIView * previewView;
+@property (nonatomic, strong) AVCaptureSession* session;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
