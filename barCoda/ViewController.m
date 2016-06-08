1a5e291eaf36d3f91dd84d0fbce57537871bf8a8
Allen Spicer <allenspicer@gmail.com>
2016-06-08 11:25:50 -0400

    Update

--- barCoda/ViewController.m
+++ barCoda/ViewController.m
@@ -23,10 +23,22 @@
     [super viewDidLoad];
     self.session = [[AVCaptureSession alloc]init];
     self.previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
-    self.previewView = [[UIView alloc]initWithFrame:CGRectMake(20, 20, self.view.bounds.size.width*0.9, self.view.bounds.size.height*0.9)];
+    self.previewView = [[UIView alloc]initWithFrame:CGRectMake(self.view.bounds.size.width*0.1, self.view.bounds.size.height*0.1, self.view.bounds.size.width*0.9, self.view.bounds.size.height*0.9)];
+    
     [self.view addSubview:self.previewView];
     [self.previewView.layer addSublayer:self.previewLayer];
-
+    
+    self.previewLayer.videoGravity = AVLayerVideoGravityResize;
+    
+    NSArray* devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    AVCaptureDevice* camera = nil;
+    for (AVCaptureDevice* device in devices) {
+        if (device.position == AVCaptureDevicePositionBack) {
+            camera = device;
+        }
+    }
+    NSError* error = nil;
+    AVCaptureDeviceInput* cameraInput = [[AVCaptureDeviceInput alloc]initWithDevice:camera error:&error];
 }
 
 
