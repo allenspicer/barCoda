daa6da45c85769fda36cd1ec5c6eb780d07af8f3
Allen Spicer <allenspicer@gmail.com>
2016-06-08 12:14:34 -0400

    Update

--- barCoda/ViewController.m
+++ barCoda/ViewController.m
@@ -8,11 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <AVCaptureMetadataOutputObjectsDelegate>
 
 @property (nonatomic, strong) UIView * previewView;
 @property (nonatomic, strong) AVCaptureSession* session;
 @property (nonatomic, strong) AVCaptureVideoPreviewLayer* previewLayer;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 
 @end
@@ -23,7 +24,7 @@
     [super viewDidLoad];
     self.session = [[AVCaptureSession alloc]init];
     self.previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
-    self.previewView = [[UIView alloc]initWithFrame:CGRectMake(self.view.bounds.size.width*0.1, self.view.bounds.size.height*0.1, self.view.bounds.size.width*0.9, self.view.bounds.size.height*0.9)];
+    self.previewView = [[UIView alloc]initWithFrame:CGRectMake(self.view.bounds.size.width*0.05, self.view.bounds.size.height*0.05, self.view.bounds.size.width*0.9, self.view.bounds.size.height*0.4)];
     
     [self.view addSubview:self.previewView];
     [self.previewView.layer addSublayer:self.previewLayer];
@@ -39,7 +40,49 @@
     }
     NSError* error = nil;
     AVCaptureDeviceInput* cameraInput = [[AVCaptureDeviceInput alloc]initWithDevice:camera error:&error];
+    AVCaptureMetadataOutput* output = [[AVCaptureMetadataOutput alloc]init];
+    if(cameraInput == nil){
+        NSLog(@"%@", [error localizedDescription]);
+    }else{
+        [self.session addInput:cameraInput];
+        [self.session addOutput:output];
+    }
+    
+    NSSet* potentialDataTypes = [NSSet setWithArray:
+                                 @[
+                                   AVMetadataObjectTypeQRCode,
+                                   AVMetadataObjectTypeEAN8Code,
+                                   AVMetadataObjectTypeEAN13Code,
+                                   AVMetadataObjectTypeDataMatrixCode,
+                                   ]];
+    NSMutableArray* supportedMetaDataTypes = [NSMutableArray array];
+    for (NSString* availableMetaDataType in output.availableMetadataObjectTypes){
+        if ([potentialDataTypes containsObject:availableMetaDataType]){
+            [supportedMetaDataTypes addObject: availableMetaDataType];
+        }
+    }
+    [output setMetadataObjectTypes:supportedMetaDataTypes];
+    [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [self.session startRunning];
+    
 }
+
+- (void) viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    self.previewLayer.frame = self.previewView.bounds;
+}
+
+
+
+    - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
+        if (metadataObjects.count > 0){
+        NSLog(@"%@", metadataObjects);
+            
+        self.textView.text = [metadataObjects description];
+        }
+    
+    }
+
 
 
 
